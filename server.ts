6c0f0c847c935e6fe4a5360683303ecdabc0f04c
anatolicheban <107035461+anatolicheban@users.noreply.github.com>
2023-04-28 19:26:54 +0300

    init project

--- server.ts
+++ server.ts
@@ -8,8 +8,9 @@ const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
-});
+})
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
